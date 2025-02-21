f778207e1b074eb428914c0acd549d84f88e6577
eldaniz <eldanizbabayev@proton.me>
2025-02-21 01:05:49 +0100

    fix dockerfile, fix sh file, create launch file for lab scene

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,12 +1,3 @@
-#!/bin/bash
-
-# Based on the ETH Robotics Summer school docker: 
-# https://github.com/ETHZ-RobotX/smb_docker/
-
-# If not working, first do: sudo rm -rf /tmp/.docker.xauth
-# It still not working, try running the script as root.
-
-# Default options
 DOCKER=nvblox
 DOCKERFILE=Dockerfile
 NAME=nvblox
@@ -21,15 +12,13 @@ help()
     exit 2
 }
 
-SHORT=b,n:,w:,h
-LONG=build,name:,workspace:,help
-OPTS=$(getopt -a -n run_docker --options $SHORT --longoptions $LONG -- "$@")
-echo $OPTS
+SHORT="b,n:,w:,h"
+LONG="build,name:,workspace:,help"
+OPTS=$(getopt -o $SHORT --long $LONG -- "$@")
 
 eval set -- "$OPTS"
 
-while :
-do
+while true; do
   case "$1" in
     -b | --build )
       BUILD="true"
@@ -47,7 +36,7 @@ do
       help
       ;;
     --)
-      shift;
+      shift
       break
       ;;
     *)
@@ -57,48 +46,54 @@ do
   esac
 done
 
-
-if [ "$BUILD" = true ]; then
+if [ "$BUILD" = "true" ]; then
     echo "Building docker: $DOCKERFILE as $DOCKER"
-    docker build -f $DOCKERFILE -t $DOCKER .
+    docker build -f "$DOCKERFILE" -t "$DOCKER" .
 fi
 
 XAUTH=/tmp/.docker.xauth
 
 echo "Preparing Xauthority data..."
 xauth_list=$(xauth nlist :0 | tail -n 1 | sed -e 's/^..../ffff/')
-if [ ! -f $XAUTH ]; then
+
+if [ ! -f "$XAUTH" ]; then
     if [ -n "$xauth_list" ]; then
-        echo $xauth_list | xauth -f $XAUTH nmerge -
+        echo "$xauth_list" | xauth -f "$XAUTH" nmerge -
     else
-        touch $XAUTH
+        touch "$XAUTH"
     fi
-    chmod a+r $XAUTH
+    chmod a+r "$XAUTH"
 fi
 
 echo "Done."
 echo ""
 echo "Verifying file contents:"
-file $XAUTH
+file "$XAUTH"
 echo "--> It should say \"X11 Xauthority data\"."
 echo ""
 echo "Permissions:"
-ls -FAlh $XAUTH
+ls -FAlh "$XAUTH"
 echo ""
 echo "Running docker..."
 
 docker run -it --rm \
     --env="DISPLAY=$DISPLAY" \
     --env="FRANKA_IP=$FRANKA_IP" \
-    --volume=$WORKSPACE:/root/nvblox_ws \
-    --volume=/home/$USER/data:/root/data \
+    --volume="$WORKSPACE:/root/nvblox_ws" \
+    --volume="/home/$USER/data:/root/data" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --env="XAUTHORITY=$XAUTH" \
     --volume="$XAUTH:$XAUTH" \
     --net=host \
     --privileged \
-    --name=$NAME \
-    ${DOCKER} \
+    --name="$NAME" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --env="NVIDIA_DRIVER_CAPABILITIES=all" \
+    --env="NVIDIA_VISIBLE_DEVICES=all" \
+    --device="/dev/video0" \
+    --gpus all \
+    --ipc=host \
+    "$DOCKER" \
     bash
 
 echo "Done."
